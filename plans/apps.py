e015c33208f02b56a585a82b868cc12d99c88b73
Arash Javadi <arash12javadi@gmail.com>
2025-08-28 22:01:17 +0100

    Create apps.py

---
+++ plans/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class PlansConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'plans'
+
+    def ready(self):
+        import plans.signals  # ensures signals run after migration
