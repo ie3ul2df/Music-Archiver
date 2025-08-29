aeb240c2e998dc1fce20e9ad8228a3a88b4b7b0e
Arash Javadi <arash12javadi@gmail.com>
2025-08-29 11:00:58 +0100

    added basket context to settings template

--- music_project/settings.py
+++ music_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'tracks',
     'playlist',
     'plans',
+    'basket',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_contents',
             ],
         },
     },
