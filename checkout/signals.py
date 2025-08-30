1e39e9cdda58ca824faae1d6c5fe5b75638141a2
Arash Javadi <ie3ul2df@students.codeinstitute.net>
2025-08-30 17:56:32 +0100

    update order total on OrderItem save/delete

---
+++ checkout/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import OrderItem
+
+@receiver(post_save, sender=OrderItem)
+def update_order_total_on_save(sender, instance, created, **kwargs):
+    order = instance.order
+    order.order_total = sum(item.price for item in order.items.all())
+    order.save()
+
+
+@receiver(post_delete, sender=OrderItem)
+def update_order_total_on_delete(sender, instance, **kwargs):
+    order = instance.order
+    order.order_total = sum(item.price for item in order.items.all())
+    order.save()
