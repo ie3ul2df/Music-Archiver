aa7561db49c675b58055b7d4b70f05026b9d38e8
Arash Javadi <ie3ul2df@students.codeinstitute.net>
2025-09-06 13:11:09 +0100

    Register Order and OrderItem in Django admin with inline items and readonly fields

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,15 @@
+# checkout/admin.py
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ("price",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("order_number", "email", "order_total", "date")
+    readonly_fields = ("order_number", "date", "order_total", "stripe_pid", "original_basket")
+    inlines = (OrderItemInline,)
